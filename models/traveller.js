3949ff694dd27ce492a2845463fc330ff083588b
Antonio Lorusso <mitictonylor@gmail.com>
2020-04-15 18:02:44 +0100

    pass test for calculate the total distance of the journeys

--- models/traveller.js
+++ models/traveller.js
@@ -3,14 +3,14 @@ const Traveller = function(journeys) {
 };
 
 Traveller.prototype.getJourneyStartLocations = function() {
-return this.journeys.map(function(journey) {
-   return journey.startLocation} )
+  return this.journeys.map(function(journey) {
+     return journey.startLocation} )
 };
 
 Traveller.prototype.getJourneyEndLocations = function () {
   return this.journeys.map(function(journey) {
      return journey.endLocation} )
-  };
+};
 
 Traveller.prototype.getJourneysByTransport = function (transport) {
  return this.journeys.filter((journey) => {
@@ -19,13 +19,15 @@ Traveller.prototype.getJourneysByTransport = function (transport) {
 };
 
 Traveller.prototype.getJourneysByMinDistance = function (minDistance) {
-return this.journeys.filter(function(journey) {
-  return journey.distance > minDistance
-})
+  return this.journeys.filter(function(journey) {
+    return journey.distance > minDistance
+  })
 };
 
 Traveller.prototype.calculateTotalDistanceTravelled = function () {
-
+  return this.journeys.reduce(function(tot, journey) {
+    return tot += journey.distance
+  }, 0)
 };
 
 Traveller.prototype.getUniqueModesOfTransport = function () {
